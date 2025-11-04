b4a23433b0bb3234f743afaaaef15471fd8b3a0a
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2025-11-04 13:58:29 +0530

    feat(user): show errors on failed validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def update
-    @user = User.find(params[:id]).update(user_params)
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
